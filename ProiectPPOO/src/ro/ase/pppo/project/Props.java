f52ceb74d40da3542d6f3c9049717b67096346dd
Albert <albertnastasoiu@gmail.com>
2015-11-02 21:47:32 +0200

    props delete comms

--- ProiectPPOO/src/ro/ase/pppo/project/Props.java
+++ ProiectPPOO/src/ro/ase/pppo/project/Props.java
@@ -6,57 +6,28 @@ import java.util.Properties;
 import java.util.Set;
 
 public class Props {
-	//TODO nu cred ca e facut pentru multithread
+	// TODO nu cred ca e facut pentru multithread
 	private final Properties configProp = new Properties();
-	private Props(){
+
+	private Props() {
 		InputStream in = this.getClass().getClassLoader().getResourceAsStream("config.properties");
-		System.out.println("Read all properties from file");
-		
-		try{
+		try {
 			configProp.load(in);
-		}catch(IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
+			System.out.println("Properties file not found");
 		}
 	}
-	
-	private static class LazyHolder{
+
+	private static class LazyHolder {
 		private static final Props INSTANCE = new Props();
 	}
-	
-	protected static Props getInstance(){
+
+	protected static Props getInstance() {
 		return LazyHolder.INSTANCE;
 	}
-	
-	public String getProperty(String key){
+
+	public String getProperty(String key) {
 		return configProp.getProperty(key);
 	}
-	
-	public Set<String> getAllPropertyNames(){
-		return configProp.stringPropertyNames();
-	}
-	
-	public boolean containsKey (String key){
-		return configProp.containsKey(key);
-	}
-	
-	//TODO Singleton to read from props
-//	public static Properties accessProp() throws FileNotFoundException{
-//		
-//		Properties prop = new Properties();
-//		ClassLoader loader = Thread.currentThread().getContextClassLoader();           
-//		InputStream inputStream = loader.getResourceAsStream("config.properties");
-//		 
-//		if (inputStream != null) {
-//			try {
-//				prop.load(inputStream);
-//				return prop;
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//		} else {
-//			throw new FileNotFoundException("property file not found.");
-//		}
-//		return prop;
-//	}
 }
